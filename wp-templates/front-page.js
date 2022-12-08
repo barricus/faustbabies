69b71d05f1bbdd7c774bc279059c3e592097b9b5
Barry Craig <barry.craig@wpengine.com>
2022-12-08 17:33:48 -0600

    Again

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -34,11 +34,8 @@ export default function Component() {
           <Hero title={'Front Page'} />
           <div className="text-center">
             <p>This page is utilizing the "front-page" WordPress template.</p>
-            <code>./wp-templates/front-page.js HA HA HA</code>
+            <code>./wp-templates/front-page.js</code>
           </div>
-        </Container>
-        <Container>
-
         </Container>
       </Main>
       <Footer title={siteTitle} menuItems={footerMenu} />
